55fbdeb61a2e96e1ead6966df86b5dc0c01e0c4f
heyitsiveen <heyitsiveen@gmail.com>
2026-09-26 04:59:50 +0800

    feat(top-picks): refresh top picks, 26 Sep 2026
    - Moisturizer pick: Celeteque Hydration replaces Neutrogena Hydro Boost
    - New runner-ups: Cetaphil Gentle Foaming, Luxe Organix Niacinamide 10%,
      Pond's Hydra Miracle, Beach Hut Lip SPF100, Nivea Original Care
    - Re-check all six entries against Watsons PH ratings, stock and prices
    - Point the at-a-glance line and cost note at the new moisturizer

--- get-photos.sh
+++ get-photos.sh
@@ -11,7 +11,7 @@ get(){ local id="$1"; shift
       echo "  saved  images/$id.jpg"; ok=$((ok+1)); saved+=("$id"); return; fi
   done
   rm -f "images/$id.jpg"; echo "  FAILED $id (the page loads it from the retailer when online)"; fail=$((fail+1)); }
-echo "Fetching 34 product photos..."
+echo "Fetching 40 product photos..."
 get klued "https://storage.skinsort.com/owajo7akkfj64y1bhb5fcxry36vv"
 get cerave "https://medias.watsons.com.ph/publishing/50053257-v75cguLk-zoom.png?version=1762914671"
 get cosrx "https://medias.watsons.com.ph/publishing/COSRX_Image1_50044422-eHzI0fRd-zoom.jpg?version=1785153905"
@@ -46,6 +46,12 @@ get mediheal "https://medias.watsons.com.ph/publishing/Mediheal_Image1_50056954-
 get dhc "https://www.wownippon.com/cdn/shop/files/dhc-hydrating-lip-cream-1-5g-japanese-lip-balm.jpg?v=1786951718"
 get luxe_lipscreen "https://medias.watsons.com.ph/publishing/WTCPH-50047764-front-zoom.jpg?version=1734346545"
 get lipice "https://medias.watsons.com.ph/publishing/WTCPH-50042100-front-zoom.jpg"
+get cetaphil_gfc "https://medias.watsons.com.ph/publishing/Watson%20BAU%20PDP-GFC-236ml-2-3nCAd9Gh-zoom.png?version=1776840986" "https://medias.watsons.com.ph/publishing/Watson%20BAU%20PDP-GFC-236ml-3-L8xNfwhg-zoom.png?version=1776840993"
+get luxe_nia10 "https://medias.watsons.com.ph/publishing/50028463_LOWhiteningRepairSerum_%20%20%281%29-ZnDNkmbR-zoom.jpg?version=1772588713" "https://medias.watsons.com.ph/publishing/50028463_LOWhiteningRepairSerum_%20%20%283%29-8OWNAarE-zoom.jpg?version=1772588629"
+get celeteque_moist "https://medias.watsons.com.ph/publishing/WTCPH-10081411-front-zoom.jpg?version=1733995085" "https://medias.watsons.com.ph/publishing/WTCPH-10081410-front-zoom.jpg?version=1733994861"
+get ponds_hydra "https://medias.watsons.com.ph/publishing/WTCPH-50054015-front-zoom.jpg?version=1750265451" "https://medias.watsons.com.ph/publishing/WTCPH-50054015-side-zoom.jpg?version=1750265456"
+get beachhut_lip "https://medias.watsons.com.ph/publishing/Beach%20Hut_Image1_50046460-MQYXsMuB-zoom.png?version=1762571547" "https://medias.watsons.com.ph/publishing/Beach%20Hut_Image3_50046460-srevf1tE-zoom.png?version=1762571553"
+get nivea_lip "https://medias.watsons.com.ph/publishing/10026702-5D20c0JI-zoom.JPG?version=1763175290" "https://medias.watsons.com.ph/publishing/WTCPH-10026702-side-zoom.jpg?version=1721932289"
 json="{"; sep=""
 for f in images/*.jpg; do [ -e "$f" ] || continue; id="$(basename "$f" .jpg)"; json="$json$sep\"$id\":\"$id.jpg\""; sep=","; done
 json="$json}"
